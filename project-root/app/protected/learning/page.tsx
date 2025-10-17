8b211d405697bf92b967fbe4624e78ed89e869dd
oroe0 <owen.roe@icloud.com>
2025-10-17 08:54:55 -1000

    Updated the learning page

--- project-root/app/protected/learning/page.tsx
+++ project-root/app/protected/learning/page.tsx
@@ -1,7 +1,59 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { onAuthStateChanged, signOut, User } from 'firebase/auth';
+import { auth } from '@/firebase';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+
 export default function Learning() {
+    const router = useRouter();
+    const [user, setUser] = useState<User | null>(null);
+
+    // Auth
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (!currentUser) router.push('/login');
+      else setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, [router]);
+
+
+
     return (
-        <div>
-            <p>hello world</p>
+        <div className="min-h-screen bg-gradient-to-r from-yellow-200 via-stone-400 to-blue-900 text-gray-900 font-petrona">
+            {/* Fixed Top Bar */}
+            <header className="fixed top-0 w-full z-50 flex justify-between items-center px-6 py-4 bg-white/80 shadow-md backdrop-blur">
+                {/* Logo */}
+                <a href="/protected" className="flex items-center space-x-2">
+                    <img src="/large-logo.png" className="w-12 h-12 rounded-full" />
+                    <span className="pl-3 font-bold font-oldenburg text-lg text-blue-900">Mock Trial Online Trainer</span>
+                </a>
+
+                <div className="flex items-center space-x-3">
+                {user && <p className="text-blue-900 font-semibold">{user.email}</p>}
+                <button
+                    onClick={async () => { await signOut(auth); router.push('/') }}
+                    className="px-4 py-2 bg-red-500 rounded-full text-white font-semibold hover:bg-red-400 transition"
+                >
+                    Logout
+                </button>
+                </div>
+            </header>
+
+
+            {/* main area */}
+            <div className="flex flex-col items-center text-center pt-24">
+
+                <h1 className='text-blue-950 text-5xl font-bold font-oldenburg m-9'>Learning Mode</h1>
+
+                <div className=''>
+                    <p>This page is a work in progress. It will be completed soon.</p>
+                </div>
+            </div>
         </div>
     )
 }
