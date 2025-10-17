e867efb5c688ee1fd5f5f06f5b00f84824db603e
oroe0 <owen.roe@icloud.com>
2025-10-16 20:24:43 -1000

    fixed next.config and package

--- project-root/next.config.ts
+++ project-root/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: { unoptimized: true },
-  // if you’re deploying to a subpath (like username.github.io/repo-name)
-  basePath: '/MTOT',
-  assetPrefix: '/MTOT/',
 };
 
 export default nextConfig;
